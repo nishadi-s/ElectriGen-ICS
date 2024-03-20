cf5351f96f415bf625bcb3c2af1394f3b555c3aa
Senith Udara <senithudara0000@gmail.com>
2024-03-20 22:41:22 +0530

    creating the side navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
+import React from "react";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard.jsx";
+import MyProfile from "./pages/MyProfile.jsx";
+import Products from "./pages/Products.jsx";
+import Materials from "./pages/Materials.jsx";
+import Analytics from "./pages/Analytics.jsx";
+import Production from "./pages/Production.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/Materials" element={<Materials />} />
+          <Route path="/Production" element={<Production />} />
+          <Route path="/Analytics" element={<Analytics />} />
+          <Route path="/MyProfile" element={<MyProfile />} />
+        </Routes>
+      </Navbar>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
