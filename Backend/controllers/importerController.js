1db06fea2395310a5405d940ceb77d2c14e39c93
Shanali Rajakaruna <it22095558@my.sliit.lk>
2024-03-30 10:28:29 +0530

    implementing importer crud

---
+++ Backend/controllers/importerController.js
@@ -0,0 +1,83 @@
+const Importer=require('../models/importerModel')
+const mongoose=require('mongoose')
+
+//get all importers
+const getImporters=async(req,res)=>{
+    const importer=await Importer.find({}).sort({createdAt: -1})
+
+    res.status(200).json(importer)
+}
+
+//get a single importer
+const getImporter=async(req,res)=>{
+    const { id }=req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    const importer=await Importer.findById(id)
+
+    if(!importer){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    res.status(200).json(importer)
+}
+
+//create new importer
+const createImporter=async(req,res)=>{
+    const{importerID, importerName, address, contactNumber, email}=req.body
+
+    //add doc to db
+    try{
+        const importer=await Importer.create({importerID, importerName, address, contactNumber, email})
+        res.status(200).json(importer)
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+}
+
+//delete a importer
+const deleteImporter=async(req,res)=>{
+    const { id }=req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    const importer=await Importer.findOneAndDelete({_id:id})
+
+    if(!importer){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    res.status(200).json(importer)
+}
+
+//update a importer
+const updateImporter=async(req,res)=>{
+    const { id }=req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    const importer=await Importer.findOneAndUpdate({_id:id},{
+        ...req.body
+    })
+
+    if(!importer){
+        return res.status(404).json({error:'No such importer'})
+    }
+
+    res.status(200).json(importer)
+}
+
+module.exports={
+    getImporters,
+    getImporter,
+    createImporter,
+    deleteImporter,
+    updateImporter
+}
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const productRoutes = require("./routes/products");
 const exportRoutes=require('./routes/export')
+const importerRoutes=require('./routes/importer')
 
 //express app
 const app = express();
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/products", productRoutes);
 app.use('/api/export', exportRoutes)
+app.use('/api/importer', importerRoutes)
 
 //connect to DB
 mongoose
@@ -34,6 +36,13 @@ mongoose
   });
 
   //middleware-export
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+//middleware-importer
 app.use(express.json())
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
