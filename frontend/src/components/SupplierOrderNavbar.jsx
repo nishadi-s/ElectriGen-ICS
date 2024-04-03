f54da081107d02ec60344a6e6c6ad4a3a516d345
nishadi-s <IT22542038@my.sliit.lk>
2024-04-03 21:21:10 +0530

    change supplierorder navbar active class name

--- frontend/src/components/SupplierOrderNavbar.jsx
+++ frontend/src/components/SupplierOrderNavbar.jsx
@@ -69,7 +69,7 @@ const Navbar = ({ children }) => {
             to={item.path}
             key={index}
             className="link"
-            activeclassName="active"
+            activeclassname="active"
           >
             <div className="icon">{item.icon}</div>
             <div
