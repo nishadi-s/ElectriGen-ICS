147b0021c36cb54e9507c973aa51a3170cb9f43d
nishadi-s <IT22542038@my.sliit.lk>
2024-04-10 12:45:20 +0530

    import SupplierContextProvider and SupplierOrderContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
  //Nishadi
  import { SupplierOrderContextProvider } from "./context/SupplierOrderContext";
+ import { SupplierContextProvider } from "./context/SupplierContext";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SupplierOrderContextProvider>
-    <App />
+      <SupplierContextProvider>
+      <App />
+      </SupplierContextProvider>
+   
     </SupplierOrderContextProvider>
    
   </React.StrictMode>
