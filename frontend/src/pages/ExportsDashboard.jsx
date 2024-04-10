ca626d2c577d8c41bf0daa8a934e9f611ec0b010
Shanali Rajakaruna <it22095558@my.sliit.lk>
2024-04-10 21:49:13 +0530

    implementing export search

--- frontend/src/pages/ExportsDashboard.jsx
+++ frontend/src/pages/ExportsDashboard.jsx
@@ -2,15 +2,16 @@ import '../exports.css';
 import React from "react";
 import { useEffect } from "react";
 import { useExportsContext } from "../hooks/useExportsContext";
-import { useImportersContext } from "../hooks/useImportersContext";
+//import { useImportersContext } from "../hooks/useImportersContext";
+import ExportSearch from '../components/ExportSearch'; 
 
 //components
-import ExportDetails from '../components/ExportDetails'
-import ImporterDetails from '../components/ImporterDetails'
+//import ExportDetails from '../components/ExportDetails'
+//import ImporterDetails from '../components/ImporterDetails'
 
 const ExportsDashboard=()=>{
-  const {exports,dispatch:exportsDispatch} = useExportsContext()
-  const {importers,dispatch:importersDispatch} = useImportersContext()
+  const {dispatch:exportsDispatch} = useExportsContext()
+  //const {importers,dispatch:importersDispatch} = useImportersContext()
 
   useEffect(()=>{
     const fetchExports=async()=>{
@@ -24,35 +25,30 @@ const ExportsDashboard=()=>{
     }
 
     
-    const fetchImporters=async()=>{
-      const response=await fetch('/api/importer')
-      const json=await response.json()
+    // const fetchImporters=async()=>{
+    //   const response=await fetch('/api/importer')
+    //   const json=await response.json()
 
-      if(response.ok){
-        importersDispatch({type: 'SET_IMPORTERS',payload:json})
-      }
+    //   if(response.ok){
+    //     importersDispatch({type: 'SET_IMPORTERS',payload:json})
+    //   }
 
-    }
+    // }
 
     fetchExports()
-    fetchImporters()
-  }, [exportsDispatch, importersDispatch])
+    //fetchImporters()
+  }, [exportsDispatch])
 
-    return (
+
+  return (
+    <div className="ex-dashboard">
       
-      <div className="ex-dashboard">
-        <div className="exports">
-        <h2>Export Orders</h2><br></br>
-          {exports && exports.map((exportt)=>(
-            <ExportDetails key={exportt._id} exportt={exportt}/>           
-          ))}
-        <h2>Importers</h2><br></br>
-          {importers && importers.map((importer)=>(
-            <ImporterDetails key={importer._id} importer={importer}/>           
-          ))}
-        </div>
+      <div className="exports">
+        <h2>Export Orders</h2><br />
+        <ExportSearch />
       </div>
-    );
+    </div>
+  );
 };
-      
+
 export default ExportsDashboard;
