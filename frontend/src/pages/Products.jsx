fc7c5dc52975a677ef58e494874fbed5db80857b
Senith Udara <senithudara0000@gmail.com>
2024-04-10 23:19:49 +0530

    creating the search function

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useEffect } from "react";
+// Products.jsx
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../senith.css";
-
-//components
 import ProductDetails from "../components/ProductDetails";
 import { useProductContext } from "../hooks/useProductsContext";
+import ProductSearch from "../components/ProductSearch"; // Import ProductSearch component
 
 const Products = () => {
   const { products, dispatch } = useProductContext();
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -17,20 +17,32 @@ const Products = () => {
 
       if (response.ok) {
         dispatch({ type: "SET_PRODUCTS", payload: json });
+        setFilteredProducts(json); // Initialize filteredProducts with all products
       }
     };
 
     fetchProducts();
   }, [dispatch]);
 
+  const handleSearch = (term) => {
+    const filtered = products.filter((product) => {
+      const productName = product.name.toLowerCase();
+      const itemCode = product.itemCode.toLowerCase();
+      return productName.includes(term) || itemCode.includes(term);
+    });
+
+    setFilteredProducts(filtered);
+  };
+
   return (
     <div className="home">
+      <ProductSearch onSearch={handleSearch} />{" "}
+      {/* Render ProductSearch component */}
       <div className="products">
-        {products &&
-          products.map((product) => (
-            <ProductDetails key={product._id} product={product} />
-          ))}
-        <Link to={`/AddProducts`} className="edit-link">
+        {filteredProducts.map((product) => (
+          <ProductDetails key={product._id} product={product} />
+        ))}
+        <Link to="/AddProducts" className="edit-link">
           <button>Add a new Product</button>
         </Link>
       </div>
