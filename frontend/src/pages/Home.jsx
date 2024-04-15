bf234c44418b75541d4718bcf43077cb5146087d
Dinithi Wickramaarachchi <dinithiwr@gmail.com>
2024-04-16 01:53:12 +0530

    commit after rebase

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const HomePage = () => {
+const Home = () => {
   return (
     <div>
       <h1>Welcome to Our Website</h1>
@@ -9,4 +9,4 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+export default Home;
